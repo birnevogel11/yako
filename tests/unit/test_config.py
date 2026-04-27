058b520d082d969f15ca8942f6bb7b208c869043
dougle <github@gigaclear.33mail.com>
2026-04-27 20:01:39 +0100

    add yaml include tests

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -2,7 +2,7 @@ import contextlib
 import os
 from pathlib import Path
 
-from yako.config import RunnerMode, YakoInputConfig
+from yako.config import RunnerMode, YakoInputConfig, init_config
 
 
 @contextlib.contextmanager
@@ -31,3 +31,35 @@ def test_load_config(tmp_path: Path) -> None:
     with cd(tmp_path):
         config = YakoInputConfig(runner_mode=RunnerMode.Local)
         assert config.runner_mode.value == "local"
+
+
+def test_load_config_include(tmp_path: Path) -> None:
+    config_path = tmp_path / "yako.yaml"
+    config_path.write_text("given: !inc yako_given.yaml")
+
+    config_path = tmp_path / "yako_given.yaml"
+    config_path.write_text("vars:\n  test_var: this value")
+
+    with cd(tmp_path):
+        config = init_config()
+        assert config.given.extra_vars["test_var"] == "this value"
+
+
+def test_load_config_include_glob(tmp_path: Path) -> None:
+    config_path = tmp_path / "yako.yaml"
+    config_path.write_text("""
+        given:
+          vars:
+            users: !inc user_*.yaml
+    """)
+
+    for i in range(3):
+        config_path = tmp_path / f"user_{i}.yaml"
+        config_path.write_text(f"""
+            username: "user_{i}"
+            index: {i}
+        """)
+
+    with cd(tmp_path):
+        config = init_config()
+        assert len(config.given.extra_vars["users"]) == 3
